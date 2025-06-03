b2885a0592e4d7930cb4ced087ae1b9fca165a7b
Krishnakumar T <krishnakumarpeni199@gmail.com>
2025-06-03 16:36:24 +0530

    fix in the export file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output :"export",
+   // Optional additional configs:
+  images: {
+    unoptimized: true, // Required for static exports
+  },
+  trailingSlash: true, // Recommended for static exports
 };
+
 
 export default nextConfig;
